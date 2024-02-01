487f215ad6bc86413084ca036ec9bddf9ded330a
d_ego <dvegor@gmail.com>
2024-02-01 23:42:00 +0400

    task2

---
+++ CustomEnumerable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class CustomEnumerable : IEnumerable<int>
+    {
+
+        public IEnumerator<int> GetEnumerator() => new CustomEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    }
+}
